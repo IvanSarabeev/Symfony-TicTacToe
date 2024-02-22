fbe617a46cdeb2a8530f9714771b2d1aea90ff28
Ivan Sarabeev <ivan.sarabeev@mypos.corp>
2024-02-22 17:05:46 +0200

    feat: created homepage view & controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function homePage(): Response
+    {
+        return $this->render('views/homepage.html.twig');
+    }
+}
