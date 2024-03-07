c09e22f4951d3ee4fb5585dcd06bfa7bd1fb4129
Ivan Sarabeev <ivan.sarabeev@mypos.corp>
2024-03-07 18:00:21 +0200

    feat: added mailer feature

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,16 +5,27 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegistrationController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager,
+        MailerInterface $mailer
+    ): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -31,8 +42,19 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
+            $email = (new TemplatedEmail())
+                ->from('test@example.com')
+                ->to($user->getEmail())
+                ->subject('Welcome to the TicTacToe World')
+                ->htmlTemplate('email/welcome.html.twig')
+                ->context([
+                    'user' => $user
+                ])
+            ;
+
+            $mailer->send($email);
+
             $this->addFlash('success', 'Account created!');
-            // do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_login');
         }
